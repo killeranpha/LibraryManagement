09f99171ac5fa08a3e1ab47db8399e2668c3efeb
Ngô Duy Nguyên <windows.com.vn@gmail.com>
2019-07-12 14:16:06 +0700

    Khởi tạo ban đầu cho Menu, Tác giả, Thể loại, Nhà xuất bản. Nhóm thể loại

---
+++ app/Http/Controllers/TacGiaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TacGia;
+use Illuminate\Http\Request;
+
+class TacGiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TacGia  $tacGia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TacGia $tacGia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TacGia  $tacGia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TacGia $tacGia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TacGia  $tacGia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TacGia $tacGia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TacGia  $tacGia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TacGia $tacGia)
+    {
+        //
+    }
+}
---
+++ database/factories/NhomTheLoaiFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\NhomTheLoai;
+use Faker\Generator as Faker;
+
+$factory->define(NhomTheLoai::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
